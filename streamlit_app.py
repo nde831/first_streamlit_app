a04cbe04f15bf0e3719f1c11f84b5592552c1fee
nde831 <146370611+nde831@users.noreply.github.com>
2023-09-28 13:04:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Health Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach, & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
