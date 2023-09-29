c16dc8cee0af3e1109fe576dd09822ef4d6b62b0
nde831 <146370611+nde831@users.noreply.github.com>
2023-09-29 10:04:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.text('Why aren't my changes showing')  
+streamlit.text('WTF')  
 
 import pandas
 
